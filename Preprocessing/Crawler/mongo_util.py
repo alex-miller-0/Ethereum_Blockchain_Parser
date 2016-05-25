1c8a0971553e04d410e5938318dc37a897047b81
Alex Miller <asmiller1989@gmail.com>
2016-05-24 22:14:05 -0700

    more pep8

--- Preprocessing/Crawler/mongo_util.py
+++ Preprocessing/Crawler/mongo_util.py
@@ -7,84 +7,87 @@ COLLECTION = "transactions"
 
 def initMongo(client):
     """
-	Given a mongo client instance, create db/collection if either doesn't exist
+    Given a mongo client instance, create db/collection if either doesn't exist
 
-	Parameters:
-	-----------
-	client <mongodb Client>
+    Parameters:
+    -----------
+    client <mongodb Client>
 
-	Returns:
-	--------
-	<mongodb Client>
-	"""
+    Returns:
+    --------
+    <mongodb Client>
+    """
     db = client[DB_NAME]
     try:
         db.create_collection(COLLECTION)
-	except Exception as e:
-		pass
-	try:										# Create an index on the block number so duplicate records cannot be made
-		db[COLLECTION].create_index([("number", pymongo.DESCENDING)], unique=True)
-	except Exception as e:
-		pass
+    except:
+        pass
+    try:
+        # Index the block number so duplicate records cannot be made
+        db[COLLECTION].create_index(
+			[("number", pymongo.DESCENDING)],
+			unique=True
+		)
+    except:
+        pass
 
-	return db[COLLECTION]
+    return db[COLLECTION]
 
 
 def insertMongo(client, d):
-	"""
-	Insert a document into mongo client with collection selected.
+    """
+    Insert a document into mongo client with collection selected.
 
-	Params:
-	-------
-	client <mongodb Client>
-	d <dict>
+    Params:
+    -------
+    client <mongodb Client>
+    d <dict>
 
-	Returns:
-	--------
-	error <None or str>
-	"""
-	try:
-		result = client.insert_one(d)
-		return None
-	except Exception as err:
-		return err
+    Returns:
+    --------
+    error <None or str>
+    """
+    try:
+        client.insert_one(d)
+        return None
+    except Exception as err:
+        return err
 
 
 def highestBlock(client):
-	"""
-	Get the highest numbered block in the collection.
+    """
+    Get the highest numbered block in the collection.
 
-	Params:
-	-------
-	client <mongodb Client>
+    Params:
+    -------
+    client <mongodb Client>
 
-	Returns:
-	--------
-	<int>
-	"""
-
-	n = client.find_one(sort=[("number", pymongo.DESCENDING)])
-	if not n:									# If the database is empty, the highest block # is 0
-		return 0
-	assert "number" in n, "Highest block is incorrectly formatted"
-	return n["number"]
+    Returns:
+    --------
+    <int>
+    """
+    n = client.find_one(sort=[("number", pymongo.DESCENDING)])
+    if not n:									# If the database is empty, the highest block # is 0
+        return 0
+    assert "number" in n, "Highest block is incorrectly formatted"
+    return n["number"]
 
 
 def makeBlockQueue(client):
-	"""
-	Form a queue of blocks that are recorded in mongo.
+    """
+    Form a queue of blocks that are recorded in mongo.
 
-	Params:
-	-------
-	client <mongodb Client>
+    Params:
+    -------
+    client <mongodb Client>
 
-	Returns:
-	--------
-	<deque>
-	"""
-	queue = deque()
-	all_n = client.find({}, {"number":1, "_id":0},
-			sort=[("number", pymongo.ASCENDING)])
-	for i in all_n:
-		queue.append(i["number"])
-	return queue
+    Returns:
+    --------
+    <deque>
+    """
+    queue = deque()
+    all_n = client.find({}, {"number":1, "_id":0},
+    		sort=[("number", pymongo.ASCENDING)])
+    for i in all_n:
+        queue.append(i["number"])
+    return queue
